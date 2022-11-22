828345db431a9c8b8b1c33b0c959cddf8538d894
Dmitriy Goldner <keisoko@gmail.com>
2022-11-22 18:38:19 -0500

    Add sum_list_operation variable Add sum_list_operation variable to differ list summation

--- calculator_with_pattern_matching.py
+++ calculator_with_pattern_matching.py
@@ -31,8 +31,9 @@ def execute_main():
     second_number = 10
     print(calculate_value(math_operation, first_number, second_number))
 
+    sum_list_operation = "addition"
     list_of_numbers_to_sum = [27, 35, 48, 60]
-    print(calculate_value(math_operation, *list_of_numbers_to_sum))
+    print(calculate_value(sum_list_operation, *list_of_numbers_to_sum))
 
     root_operation = "cube root"
     number_to_be_rooted = 729
